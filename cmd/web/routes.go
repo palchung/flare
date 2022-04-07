671b2021a2d594d7b61961bee75342b99874ecb7
Pal Chung <palchung@gmail.com>
2022-04-07 08:04:42 -0700

    add assets

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,5 +18,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	mux.Handle("/assets/*", http.StripPrefix("/assets", fileServer))
+
 	return mux
 }
